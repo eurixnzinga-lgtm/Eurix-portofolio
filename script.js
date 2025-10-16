8fadb03a3c147e5906066d88f9494221568f1639
eurixnzinga-lgtm <eurixnzinga@gmail.com>
2025-10-17 00:57:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
